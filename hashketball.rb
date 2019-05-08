4afc781bb6d312f407314582ec2947a58efdf2ce
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-08 12:05:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -160,7 +160,10 @@ def find_the_team(team_name)
   teams.find {|team| team.fetch(:team_name) == team_name}
 end
 
-
+def team_colors(team_name)
+  team = find_the_team(team_name)
+  team.fetch(:colors)
+end
 
 
 
